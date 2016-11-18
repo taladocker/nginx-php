6f514d8f05b9ab247032e5ef008caeec22a9c49f
Huy Nguyen <luk4hn@gmail.com>
2016-11-18 09:32:12 +0700

    start.sh: enable worker base on TK_IS_WORKER env var

--- start.sh
+++ start.sh
@@ -2,12 +2,27 @@
 
 # enable xdebug if ENV variable TIKI_XDEBUG_ENABLED == 1
 _init_xdebug() {
-  echo ":: initializing xdebug config"
   local _xdebug_enableb=${TIKI_XDEBUG_ENABLED:-0}
+  echo ":: initializing xdebug config (_xdebug_enableb=${_xdebug_enableb})"
 
   if [[ $_xdebug_enableb == 1 ]] ; then
     ln -svf /etc/php/7.0/mods-available/xdebug.ini /etc/php/7.0/cli/conf.d/20-xdebug.ini
     ln -svf /etc/php/7.0/mods-available/xdebug.ini /etc/php/7.0/fpm/conf.d/20-xdebug.ini
+  fi
+}
+
+# if ENV variable TK_IS_WORKER == 1
+# -> only start worker process (dont start nginx, php-fpm)
+_init_worker() {
+  local _is_worker=${TK_IS_WORKER:-0}
+  echo ":: initializing worker config (_is_worker=${_is_worker})"
+
+  if [[ $_is_worker == 1 ]] ; then
+    sed -i 's#autostart=.*#autostart=false#g' /etc/supervisord.conf  # dont start nginx, php-fpm
+    # include worker config
+    [[ -d /etc/supervisor.d ]] || mkdir -v /etc/supervisor.d
+    grep -q include /etc/supervisord.conf \
+    || echo -e "[include]\nfiles = /etc/supervisor.d/*.conf\n" >> /etc/supervisord.conf
   fi
 }
 
@@ -22,5 +37,6 @@ if [[ -n "$@" ]]; then
   $@
 else
   _init_xdebug  # for corveralls.io ...
+  _init_worker
   exec_supervisord
 fi
